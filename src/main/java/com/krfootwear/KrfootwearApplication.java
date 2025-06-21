44115d45c22dad7d007c1ef4e66addfbdf73ae2d
khadar099 <khadar3099@gmail.com>
2025-06-21 07:29:38 +0530

    first commit

---
+++ src/main/java/com/krfootwear/KrfootwearApplication.java
@@ -0,0 +1,13 @@
+package com.krfootwear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrfootwearApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KrfootwearApplication.class, args);
+	}
+
+}
