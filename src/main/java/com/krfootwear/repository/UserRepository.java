1386a06bbcb1c59723199d3eede73d0f4ee45c12
Khadar Basha <89116722+khadar099@users.noreply.github.com>
2025-06-21 22:12:00 +0530

    Create UserRepository.java

---
+++ src/main/java/com/krfootwear/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.krfootwear.repository;
+
+import com.krfootwear.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	Optional<User> findByEmail(String email);
+}
