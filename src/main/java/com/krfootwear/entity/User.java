951b537e1ea4438185302256df5a29ef4adb9961
Khadar Basha <89116722+khadar099@users.noreply.github.com>
2025-06-21 22:35:39 +0530

    Update User.java

--- src/main/java/com/krfootwear/entity/User.java
+++ src/main/java/com/krfootwear/entity/User.java
@@ -1,16 +1,17 @@
 package com.krfootwear.entity;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 @Entity
+@Data  // Lombok annotation that generates getters, setters, toString, equals, and hashCode
 public class User {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
 
-	private String username;
-	private String email;
-	private String password;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	// Getters and Setters
+    private String username;
+    private String email;
+    private String password;
 }
